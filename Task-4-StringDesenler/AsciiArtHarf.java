6f4e44fdbd1980fe958d4e951461c143bad70cb9
Sevvalyildizz <250541025@firat.edu.tr>
2025-10-23 19:59:24 +0300

    Update AsciiArtHarf.java

--- Task-4-StringDesenler/AsciiArtHarf.java
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -1,1 +1,14 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+        System.out.println("ASCII ART - Baş Harf Ş:");
+        System.out.println("  SSSSS ");
+        System.out.println(" S     S");
+        System.out.println("  SSSSS ");
+        System.out.println("       S");
+        System.out.println("  SSSSS ");
+        System.out.println("     Ş  ");
+        System.out.println("   ŞŞ   ");
+    }
+}
+
 
