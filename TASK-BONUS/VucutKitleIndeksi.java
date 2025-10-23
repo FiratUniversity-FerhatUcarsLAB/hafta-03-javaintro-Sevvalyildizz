04fcbaf465c7c16c282335b6034b7db60705d80d
Sevvalyildizz <250541025@firat.edu.tr>
2025-10-23 20:57:50 +0300

    Update VucutKitleIndeksi.java

--- TASK-BONUS/VucutKitleIndeksi.java
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -6,18 +6,7 @@ public class BMIHesaplama {
         double bmi = kilo / (boy * boy);
 
         System.out.printf("Vucut Kitle Indeksiniz (BMI): %.2f%n", bmi);
-
-        // BMI değerlendirmesi
-        if (bmi < 18.5) {
-            System.out.println("Durum: Zayıf");
-        } else if (bmi < 25) {
-            System.out.println("Durum: Normal");
-        } else if (bmi < 30) {
-            System.out.println("Durum: Fazla kilolu");
-        } else {
-            System.out.println("Durum: Obez");
-        }
     }
 }
 
-
+                                            
