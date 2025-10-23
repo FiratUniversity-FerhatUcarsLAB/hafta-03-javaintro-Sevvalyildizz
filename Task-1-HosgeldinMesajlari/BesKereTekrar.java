e7a8bed4f3bdbddc6a68090037c7960b47a11b6c
Sevvalyildizz <250541025@firat.edu.tr>
2025-10-23 16:58:09 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,8 @@
+public class BesKereMesaj {
+    public static void main(String[] args) {
+        // 5 kez ekrana aynı mesajı yazdırmak için for döngüsü kullanıyoruz
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Java'ya Hoş Geldiniz");
+        }
+    }
+}
