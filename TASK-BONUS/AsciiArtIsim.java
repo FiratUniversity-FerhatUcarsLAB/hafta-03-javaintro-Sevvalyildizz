30ee4f84542babbd4bd8b5c54673c31c976ce11f
Sevvalyildizz <250541025@firat.edu.tr>
2025-10-23 20:16:49 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,10 @@
+public class AsciiSanat {
+    public static void main(String[] args) {
+        System.out.println("ASCII ART - ŞEVVAL:");
+        System.out.println("  SSSSS   EEEEE  V     V  V     V  AAAAA  L      L      ");
+        System.out.println(" S       E      V     V  V     V  A   A  L      L      ");
+        System.out.println("  SSSSS  EEEE   V     V  V     V  AAAAA  L      L      ");
+        System.out.println("       S E      V     V   V   V   A   A  L      L      ");
+        System.out.println("  SSSSS  EEEEE   VVVVV     VVV    A   A  LLLLL  LLLLL ");
+    }
+}
